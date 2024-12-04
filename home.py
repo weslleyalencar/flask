01ba1b14e4761fe4758bfaf5e4027038e7fa1a8d
Weslley Alencar <weslley_hssilva@hotmail.com>
2024-12-04 10:16:15 -0400

    quinto commit

--- home.py
+++ home.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify, json
+import requests 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, Railway!"
+@app.route('/endereco/<cep>')
+def home(cep):
+    dados = requests.get(f"https://viacep.com.br/ws/{cep}/json/")
+    return json.loads(dados.content)
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
