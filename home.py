57d859d0448708a03c81a5e36d37863838ec8da6
Weslley Alencar <weslley_hssilva@hotmail.com>
2024-12-04 09:21:29 -0400

    segundo commit

--- home.py
+++ home.py
@@ -1,7 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello World"
+@app.route('/')
+def home():
+    return "Hello, Railway!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
