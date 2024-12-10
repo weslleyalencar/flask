c4d2931ddb3002e62a258e53da99007b5bb9958e
Weslley Alencar <weslley_hssilva@hotmail.com>
2024-12-10 18:10:25 -0400

    api 3.0

--- home.py
+++ home.py
@@ -1,11 +1,20 @@
 from flask import Flask
 import api
+
 app = Flask(__name__)
 
-@app.route('/<cep>')
-def home(cep):
-    dados = api.get_cep(cep)
-    return dados
+@app.route('/')
+def home():
+    return "Bem-vindo à API! Use /get_cep/<local> para buscar informações."
+
+@app.route('/cep/<local>')
+def get_cep(local):
+    try:
+        # Chama a função da API com o parâmetro recebido na URL
+        dados = api.get_cep(local)
+        return {"resultados": dados}
+    except Exception as e:
+        return {"error": str(e)}, 500
 
 if __name__ == "__main__":
     app.run(debug=True)
